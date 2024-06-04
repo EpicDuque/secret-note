5139f78d0b5b30df6327daf82e5ee2edefa2a2ef
Pedro Duquesne <pduquesne@srgstudiospr.com>
2024-06-04 14:47:15 -0400

    Second Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../styles/output.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="flex min-h-screen flex-col p-3">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
